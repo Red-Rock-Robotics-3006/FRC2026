4a1fb09f0b99a12191edeef95a5fd000baa13c48
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2026-02-13 22:57:58 -0700

    swerve fix

--- src/main/java/frc/robot/subsystems/swerve/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/swerve/CommandSwerveDrivetrain.java
@@ -515,16 +515,18 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 if (Math.abs(controller.getRightX()) <= drivingDeadBand.getNumber()) state = DriveState.DRIVE_RESIDUAL;
                 break;
             case DRIVE_RESIDUAL:
-                SwerveDriveState driveState = this.getState();
-                if (Math.abs(driveState.Speeds.omegaRadiansPerSecond) <= 
-                    DegreesPerSecond.of(pidRotationThreshold.getNumber()).in(RadiansPerSecond)) {
-                    targetAngle = driveState.Pose.getRotation();
-                    state = DriveState.DRIVE_FACING_ANGLE;
+                if (Math.abs(controller.getRightX()) >= drivingDeadBand.getNumber()) state = DriveState.DRIVE;
+                else {
+                    SwerveDriveState driveState = this.getState();
+                    if (Math.abs(driveState.Speeds.omegaRadiansPerSecond) <= 
+                        DegreesPerSecond.of(pidRotationThreshold.getNumber()).in(RadiansPerSecond)) {
+                        targetAngle = driveState.Pose.getRotation();
+                        state = DriveState.DRIVE_FACING_ANGLE;
+                    }
                 }
                 break;
         }
     }
-
     public void resetHeading() {
         this.resetPose(
             new Pose2d(
