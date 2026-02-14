88eec2f76a05450eb86f43a4e7117b4e73df45c7
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2026-02-14 00:11:50 -0700

    added cameras, added hub poses

--- src/main/java/frc/robot/vision/Localization.java
+++ src/main/java/frc/robot/vision/Localization.java
@@ -6,13 +6,18 @@ import org.photonvision.EstimatedRobotPose;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Localization extends SubsystemBase{
-    public static final Pose2d redHub = new Pose2d();
-    public static final Pose2d blueHub = new Pose2d();
+    public static final Pose2d redHub = new Pose2d(Units.inchesToMeters(182.11), Units.inchesToMeters(158.84), Rotation2d.fromDegrees(0));
+    public static final Pose2d blueHub = new Pose2d(Units.inchesToMeters(469.11), Units.inchesToMeters(158.84), Rotation2d.fromDegrees(0));
 
     public static final Pose2d[] redLobTargets = {new Pose2d(), new Pose2d()};
     public static final Pose2d[] blueLobTargets = {new Pose2d(), new Pose2d()};
@@ -37,6 +42,40 @@ public class Localization extends SubsystemBase{
 
     public Localization() {
         super("localization");
+
+        cameras.add(new RedRockCamera("Photon-Rubik-Everything")
+            .withRobotToCameraTransform(
+                new Transform3d(
+                    new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
+                    new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0))
+                )
+            )
+        );
+        cameras.add(new RedRockCamera("Photon-Rubik-Nothing")
+            .withRobotToCameraTransform(
+                new Transform3d(
+                    new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
+                    new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0))
+                )
+            )
+        );
+
+        cameras.add(new RedRockCamera("Photon-Rubik-Goober")
+            .withRobotToCameraTransform(
+                new Transform3d(
+                    new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
+                    new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0))
+                )
+            )
+        );
+        cameras.add(new RedRockCamera("Photon-Rubik-Booger")
+            .withRobotToCameraTransform(
+                new Transform3d(
+                    new Translation3d(Units.inchesToMeters(0), Units.inchesToMeters(0), Units.inchesToMeters(0)),
+                    new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0))
+                )
+            )
+        );
     }
 
     /**
