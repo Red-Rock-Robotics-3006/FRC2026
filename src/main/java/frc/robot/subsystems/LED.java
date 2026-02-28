bd7d85d1f3045613af4011f6d164726212c16c6b
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2026-02-28 01:49:58 -0700

    larson with buffer views and controllable speed

--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -2,11 +2,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import redrocklib.logging.SmartDashboardNumber;
-import edu.wpi.first.wpilibj.simulation.AddressableLEDSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class LED extends SubsystemBase{
@@ -14,8 +14,11 @@ public class LED extends SubsystemBase{
     private static LED instance = null;
 
     private AddressableLED control = new AddressableLED(0); //TODO
-    private AddressableLEDSim controlSim; //TODO
-    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(50); //TODO
+    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(150); //TODO
+
+    private AddressableLEDBufferView left = buffer.createView(0, 49);
+    private AddressableLEDBufferView back = buffer.createView(50, 99);
+    private AddressableLEDBufferView right = buffer.createView(100, 149);
     
     public final Color INIT_YELLOW = new Color(255, 165, 0);
     public final Color OFF = new Color(0, 0, 0);
@@ -26,8 +29,9 @@ public class LED extends SubsystemBase{
     public final Color BLUE = new Color(0, 0, 255);
     public final Color RED = new Color(255, 0, 0);
     
-    private int blinkControl = 0;
-    private SmartDashboardNumber rainbowControl = new SmartDashboardNumber("led/rainbow control", 3);
+    private int loopControl = 0;
+    private SmartDashboardNumber rainbowControl = new SmartDashboardNumber("led/rainbow speed", 3);
+    private SmartDashboardNumber larsonSpeed = new SmartDashboardNumber("led/larson speed", 2);
 
     private LED() {
         super("LED");
@@ -39,9 +43,6 @@ public class LED extends SubsystemBase{
         this.control.setData(buffer);
         
         this.control.start();
-
-        controlSim = new AddressableLEDSim(control);
-        controlSim.setInitialized(true);
     }
 
     public enum RobotState {
@@ -66,19 +67,23 @@ public class LED extends SubsystemBase{
     }
 
     public void blink(Color c, int freq) {
-        if (blinkControl % freq * 2 < freq) this.setLights(c);
+        if (loopControl % freq * 2 < freq) this.setLights(c);
         else this.setLights(OFF);
     }
 
     public void blinkSetLights(Color c, int blinks, int freq) {
-        if (blinkControl < freq * blinks) {
-            if (blinkControl % freq * 2 < freq) this.setLights(c);
+        if (loopControl < freq * blinks) {
+            if (loopControl % freq * 2 < freq) this.setLights(c);
             else this.setLights(OFF);
         }
         else setLights(c);
     }
 
-    int rainbowHue = 0;
+    private int rainbowHue = 0;
+
+    public void increaseHueControl() {rainbowControl.putNumber(rainbowControl.getNumber() + 1);}
+    public void decreaseHueControl() {rainbowControl.putNumber(rainbowControl.getNumber() - 1);}
+
     public void rainbow() {
         for (var i = 0; i < buffer.getLength(); i++) {
           final var hue = (rainbowHue + (i * 180 / buffer.getLength())) % 180;
@@ -88,49 +93,53 @@ public class LED extends SubsystemBase{
         rainbowHue %= 180;
     }
 
-    public void increaseHueControl() {rainbowControl.putNumber(rainbowControl.getNumber() + 1);}
-    public void decreaseHueControl() {rainbowControl.putNumber(rainbowControl.getNumber() - 1);}
-
     private int larsonPosition = 0;
-    private SmartDashboardNumber larsonSpeed = new SmartDashboardNumber("led/larson speed", 2);
-    private int larsonDirection = (int) larsonSpeed.getNumber();
+    private int larsonDirection = 1;
     private final int LARSON_SIZE = 15;
 
     public void larson(Color c) {
-        for (int i = 0; i < buffer.getLength(); i++) {
-            buffer.setLED(i, new Color(
-                buffer.getLED(i).red * 0.5,
-                buffer.getLED(i).green * 0.5,
-                buffer.getLED(i).blue * 0.5
+        larson(left, c);
+        larson(back, c);
+        larson(right, c);
+    }
+
+    public void larson(AddressableLEDBufferView view, Color c) {
+        for (int i = 0; i < view.getLength(); i++) {
+            view.setLED(i, new Color(
+                view.getLED(i).red * 0.5,
+                view.getLED(i).green * 0.5,
+                view.getLED(i).blue * 0.5
             ));
         }
 
         for (int offset = -(LARSON_SIZE / 2); offset <= LARSON_SIZE / 2; offset++) {
             int index = larsonPosition + offset;
-            if (index < 0 || index >= buffer.getLength()) continue;
+            if (index < 0 || index >= view.getLength()) continue;
 
             double brightness = 1.0 - (Math.abs(offset) / (double)(LARSON_SIZE / 2 + 1));
 
-            buffer.setLED(index, new Color(
+            view.setLED(index, new Color(
                 c.red * brightness,
                 c.green * brightness,
                 c.blue * brightness
             ));
         }
 
-        larsonPosition += larsonDirection;
-        if (larsonPosition >= buffer.getLength() - 1) {
-            larsonPosition = buffer.getLength() - 1;
-            larsonDirection = (int) -larsonSpeed.getNumber();
-        } else if (larsonPosition <= 0) {
-            larsonPosition = 0;
-            larsonDirection = (int) larsonSpeed.getNumber();
+        if (loopControl % larsonSpeed.getNumber() == 0) {
+            larsonPosition += larsonDirection;
+            if (larsonPosition >= view.getLength() - 1) {
+                larsonPosition = view.getLength() - 1;
+                larsonDirection = -1;
+            } else if (larsonPosition <= 0) {
+                larsonPosition = 0;
+                larsonDirection = 1;
+            }
         }
     }
     
     @Override
     public void periodic() {
-        blinkControl++;
+        loopControl++;
 
         switch (this.getRobotState()) {
             case MANUAL_SHOT:
