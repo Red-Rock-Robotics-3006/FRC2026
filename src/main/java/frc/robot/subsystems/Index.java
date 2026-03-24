2400f9a1aa20ed16aed9c5a342ea86f14c2cb3fe
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2026-03-24 02:14:03 -0600

    kicker back

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -25,14 +25,14 @@ public class Index extends SubsystemBase {
     public static final boolean kEnableTuning = true;
 
     private RedRockTalon dyeRotorMotor = new RedRockTalon(31, "index-dyerotor-motor", "*");
-    // private RedRockTalon kickerMotor = new RedRockTalon(32, "index-kicker-motor", "*");
+    private RedRockTalon kickerMotor = new RedRockTalon(32, "index-kicker-motor", "*");
 
     private Slot1Configs slot1Configs = new Slot1Configs();
 
     private SmartDashboardNumber indexSpeed = new SmartDashboardNumber("index/index speed", 1).withTuningEnabled(true); //SHOULD BE RPM FOR MM VELOCITY VOLTAGE
     private SmartDashboardNumber indexReverseSpeed = new SmartDashboardNumber("index/index reverse speed", -0.3).withTuningEnabled(true);
 
-    // private SmartDashboardNumber kickerSpeed = new SmartDashboardNumber("index/kicker speed", 0.6).withTuningEnabled(true);
+    private SmartDashboardNumber kickerSpeed = new SmartDashboardNumber("index/kicker speed", 0.6).withTuningEnabled(true);
 
     private final double dyeRotorGearRatio = 45.82608144444; // 25 * 44 / 24;
     private SmartDashboardNumber indexSafePosition = new SmartDashboardNumber("index/index safe position", 0).withTuningEnabled(true);
@@ -102,7 +102,7 @@ public class Index extends SubsystemBase {
 
     public void startIndex() {
         // this.setIndexSpeed(indexSpeed.getNumber());
-        // this.setKickerSpeed(kickerSpeed.getNumber());
+        this.setKickerSpeed(kickerSpeed.getNumber());
         this.setIndexSpeed(indexSpeed.getNumber());
 
     }
@@ -113,20 +113,20 @@ public class Index extends SubsystemBase {
 
     public void stopIndex() {
         this.dyeRotorMotor.motor.setControl(new NeutralOut());
-        // this.setKickerSpeed(0);
+        this.setKickerSpeed(0);
     }
 
-    // private void setKickerSpeed(double speed) {
-    //     this.kickerMotor.motor.setControl(new DutyCycleOut(speed));
-    // }
+    private void setKickerSpeed(double speed) {
+        this.kickerMotor.motor.setControl(new DutyCycleOut(speed));
+    }
 
-    // public void startKicker() {
-    //     this.setKickerSpeed(kickerSpeed.getNumber());
-    // }
+    public void startKicker() {
+        this.setKickerSpeed(kickerSpeed.getNumber());
+    }
 
-    // public void stopKicker() {
-    //     this.setKickerSpeed(0);
-    // }
+    public void stopKicker() {
+        this.setKickerSpeed(0);
+    }
 
     public void khangaiIsAChud() {
         this.dyeRotorMotor.motor.setControl(new CoastOut());
@@ -171,8 +171,8 @@ public class Index extends SubsystemBase {
 
     public Command startIndexCommand() {
         return Commands.sequence(
-            Commands.runOnce(() -> this.startIndex(), this)//,
-            // Commands.runOnce(() -> this.startKicker())
+            Commands.runOnce(() -> this.startIndex(), this),
+            Commands.runOnce(() -> this.startKicker())
         );
     }
 
@@ -182,8 +182,8 @@ public class Index extends SubsystemBase {
 
     public Command stopIndexCommand() {
         return Commands.sequence(
-            Commands.runOnce(() -> this.stopIndex(), this)//,
-            // Commands.runOnce(() -> this.stopKicker())
+            Commands.runOnce(() -> this.stopIndex(), this),
+            Commands.runOnce(() -> this.stopKicker())
         );
     }
 
